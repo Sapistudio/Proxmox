a242e5818a29d557589a0c92628073e1f945c3db
laur sandu <Sapistudio@users.noreply.github.com>
2019-11-08 14:02:49 +0200

    Update Qemu.php

--- src/Data/Qemu.php
+++ src/Data/Qemu.php
@@ -7,10 +7,7 @@ class Qemu extends \SapiStudio\Proxmox\Request
     private $virtualMachineId   = null;
     private $nodeId             = null;
     
-    
-    /**
-     * Qemu::__construct()
-     */
+    /** Qemu::__construct()*/
     public function __construct($nodeId = null,$vmid = null)
     {
         if(!$nodeId || !$vmid){
@@ -20,132 +17,109 @@ class Qemu extends \SapiStudio\Proxmox\Request
         $this->nodeId           = $nodeId;
     }
     
-    /**
-     * Qemu::qemuVmid()
-     */
+    /** Qemu::getId()*/
+    public function getId()
+    {
+        return $this->virtualMachineId;
+    }
+    
+    /** Qemu::qemuVmid()*/
     public function qemuVmid()
     {
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId");
     }
 
-    /**
-     * Qemu::qemuStatus()
-     */
+    /** Qemu::qemuStatus()*/
     public function qemuStatus()
     {
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/status");
     }
 
-    /**
-     * Qemu::qemuCurrent()
-     */
+    /** Qemu::qemuCurrent()*/
     public function qemuCurrent()
     {
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/status/current");
     }
 
-    /**
-     * Qemu::qemuRrddata()
-     */
+    /** Qemu::qemuRrddata()*/
     public function qemuRrddata($timeframe = null)
     {
         $optional['timeframe'] = !empty($timeframe) ? $timeframe : null;
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/rrddata", $optional);
     }
 
-    /**
-     * Qemu::qemuDelete()
-     */
+    /** Qemu::qemuDelete()*/
     public function qemuDelete()
     {
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId", [], "DELETE");
     }
 
-    /**
-     * Qemu::qemuConfig()
-     */
+    /** Qemu::qemuConfig()*/
     public function qemuConfig()
     {
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/config");
     }
 
-    /**
-     * Qemu::qemuSetConfig()
-     */
+    /** Qemu::qemuSetConfig()*/
     public function qemuSetConfig($data = [])
     {
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/config", $data, 'PUT');
     }
 
-    /**
-     * Qemu::qemuCreateConfig()
-     */
+    /** Qemu::qemuCreateConfig()*/
     public function qemuCreateConfig($data = [])
     {
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/config", $data, 'POST');
     }
 
-    /**
-     * Qemu::qemuResume()
-     */
+    /** Qemu::qemuResume()*/
     public function qemuResume($data = [])
     {
         $fields = null;
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/status/resume", $data, 'POST');
     }
 
-    /**
-     * Qemu::qemuReset()
-     */
+    /** Qemu::qemuReset()*/
     public function qemuReset($data = [])
     {
         $fields = null;
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/status/reset", $data, 'POST');
     }
     
-    /**
-     * Qemu::qemuShutdown()
-     */
+    /** Qemu::qemuShutdown()*/
     public function qemuShutdown($data = [])
     {
         $fields = null;
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/status/shutdown", $data, 'POST');
     }
 
-    /**
-     * Qemu::qemuStart()
-     */
+    /** Qemu::qemuStart()*/
     public function qemuStart($data = [])
     {
         $fields = null;
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/status/start", $data, 'POST');
     }
     
-    /**
-     * Qemu::qemuStop()
-     */
+    /** Qemu::qemuStop()*/
     public function qemuStop($data = [])
     {
         $fields = null;
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/status/stop", $data, 'POST');
     }
 
-    /**
-     * Qemu::qemuSuspend()
-     */
+    /** Qemu::qemuSuspend()*/
     public function qemuSuspend($data = [])
     {
         $fields = null;
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/status/suspend", $data, 'POST');
     }
 
-    /**
-     * Qemu::qemuClone()
-     */
+    /** Qemu::qemuClone()*/
     public function qemuClone($data = [])
     {
         $fields = ['newid', 'name'];
-        $data['target'] = $this->nodeId;
+        if(!isset($data['target']))
+            $data['target'] = $this->nodeId;
         $data['full']   = 1;
         return self::Request("/nodes/$this->nodeId/qemu/$this->virtualMachineId/clone", $data, 'POST');
     }
