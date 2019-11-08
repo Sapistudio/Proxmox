1aa97ffb58c010ef4c069c20048fae6858015a28
laur sandu <Sapistudio@users.noreply.github.com>
2019-11-08 14:02:34 +0200

    Update Nodes.php

--- src/Data/Nodes.php
+++ src/Data/Nodes.php
@@ -5,16 +5,25 @@ use SapiStudio\Proxmox\Handler;
 
 class Nodes extends \SapiStudio\Proxmox\Request
 {
-    /**
-     * Nodes::listNodeQemus()
-     */
-    public function listNodeQemus($node,$applyModification=null)
+    private $nodeId             = null;
+    
+    /** Qemu::__construct()*/
+    public function __construct($nodeId = null)
     {
-        $qemus = self::Request("/nodes/$node/qemu");
+        if(!$nodeId){
+            throw new \Exception('Invalid Node constructor');
+        }
+        $this->nodeId           = $nodeId;
+    }
+    
+    /** Nodes::listNodeQemus()*/
+    public function listNodeQemus($applyModification=null)
+    {
+        $qemus = self::Request("/nodes/$this->nodeId/qemu");
         if(!$qemus || !isset($qemus->data))
             return false;
         foreach($qemus->data as $index=>$vmData){
-            $object = new Qemu($node,$vmData->vmid);
+            $object = new Qemu($this->nodeId,$vmData->vmid);
             if($applyModification)
                 $object->$applyModification();
             $return[$index] = $object;
@@ -22,58 +31,46 @@ class Nodes extends \SapiStudio\Proxmox\Request
         return $return;
     }
 
-    /**
-     * Nodes::qemuCreate()
-     */
+    /** Nodes::qemuCreate()*/
     public function qemuCreate($data = [])
     {
         $fields = ['vmid', 'name', 'ostype' => 'win10', 'ide2' => 'local:iso/win_10_64bit_trial_90days.iso,media=cdrom', 'ide0' => 'local-lvm:32', 'sockets' => 2, 'cores', 'numa' => 0, 'memory', 'net0' => 'e1000,bridge=vmbr0,tag=70', 'scsihw' => 'virtio-scsi-pci'];
-        return self::Request("/nodes/$node/qemu", $data, "POST");
+        return self::Request("/nodes/$this->nodeId/qemu", $data, "POST");
     }
 
-    /**
-     * Nodes::Tasks()
-     */
-    public function Tasks($node, $errors = null, $limit = null, $vmid = null, $start = null)
+    /** Nodes::Tasks()*/
+    public function Tasks($errors = null, $limit = null, $vmid = null, $start = null)
     {
         $optional['errors'] = !empty($errors) ? $errors : false;
         $optional['limit'] = !empty($limit) ? $limit : null;
         $optional['vmid'] = !empty($vmid) ? $vmid : null;
         $optional['start'] = !empty($start) ? $start : null;
-        return self::Request("/nodes/$node/tasks", $optional);
+        return self::Request("/nodes/$this->nodeId/tasks", $optional);
     }
 
-    /**
-     * Nodes::tasksUpid()
-     */
-    public function tasksUpid($node, $upid)
+    /** Nodes::tasksUpid()*/
+    public function tasksUpid($upid)
     {
-        return self::Request("/nodes/$node/tasks/$upid");
+        return self::Request("/nodes/$this->nodeId/tasks/$upid");
     }
 
-    /**
-     * Nodes::tasksStop()
-     */
-    public function tasksStop($node, $upid)
+    /** Nodes::tasksStop()*/
+    public function tasksStop($upid)
     {
-        return self::Request("/nodes/$node/tasks/$upid", null, "DELETE");
+        return self::Request("/nodes/$this->nodeId/tasks/$upid", null, "DELETE");
     }
 
-    /**
-     * Nodes::tasksLog()
-     */
-    public function tasksLog($node, $upid, $limit = null, $start = null)
+    /** Nodes::tasksLog()*/
+    public function tasksLog($upid, $limit = null, $start = null)
     {
         $optional['limit'] = !empty($limit) ? $limit : null;
         $optional['start'] = !empty($start) ? $start : null;
-        return self::Request("/nodes/$node/tasks/$upid/log", $optional);
+        return self::Request("/nodes/$this->nodeId/tasks/$upid/log", $optional);
     }
 
-    /**
-     * Nodes::tasksStatus()
-     */
-    public function tasksStatus($node, $upid)
+    /** Nodes::tasksStatus()*/
+    public function tasksStatus($upid)
     {
-        return self::Request("/nodes/$node/tasks/$upid/status");
+        return self::Request("/nodes/$this->nodeId/tasks/$upid/status");
     }
 }
